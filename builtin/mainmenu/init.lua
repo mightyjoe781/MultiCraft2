MultiCraft/MultiCraft

Add MultiCraft MainMenu

builtin/mainmenu/init.lua
@@ -3,7 +3,7 @@
 --
 --This program is free software; you can redistribute it and/or modify
 --it under the terms of the GNU Lesser General Public License as published by
---the Free Software Foundation; either version 2.1 of the License, or
+--the Free Software Foundation; either version 3.0 of the License, or
 --(at your option) any later version.
 --
 --This program is distributed in the hope that it will be useful,
@@ -22,7 +22,7 @@ mt_color_dark_green = "#25C191"
 
 local menupath = core.get_mainmenu_path()
 local basepath = core.get_builtin_path()
-local menustyle = core.settings:get("main_menu_style")
+local mobile = PLATFORM == "Android" or PLATFORM == "iOS"
 defaulttexturedir = core.get_texturepath_share() .. DIR_DELIM .. "base" ..
 					DIR_DELIM .. "pack" .. DIR_DELIM
 
@@ -36,26 +36,34 @@ dofile(menupath .. DIR_DELIM .. "common.lua")
 dofile(menupath .. DIR_DELIM .. "pkgmgr.lua")
 dofile(menupath .. DIR_DELIM .. "textures.lua")
 
-dofile(menupath .. DIR_DELIM .. "dlg_config_world.lua")
-dofile(menupath .. DIR_DELIM .. "dlg_settings_advanced.lua")
-dofile(menupath .. DIR_DELIM .. "dlg_contentstore.lua")
-if menustyle ~= "simple" then
-	dofile(menupath .. DIR_DELIM .. "dlg_create_world.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_create_world.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_create_world_default.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_delete_world.lua")
+
+if not mobile then
+	dofile(menupath .. DIR_DELIM .. "dlg_config_world.lua")
 	dofile(menupath .. DIR_DELIM .. "dlg_delete_content.lua")
-	dofile(menupath .. DIR_DELIM .. "dlg_delete_world.lua")
+	dofile(menupath .. DIR_DELIM .. "dlg_contentstore.lua")
 	dofile(menupath .. DIR_DELIM .. "dlg_rename_modpack.lua")
+	dofile(menupath .. DIR_DELIM .. "dlg_settings_advanced.lua")
 end
 
 local tabs = {}
 
-tabs.settings = dofile(menupath .. DIR_DELIM .. "tab_settings.lua")
-tabs.content  = dofile(menupath .. DIR_DELIM .. "tab_content.lua")
+if not mobile then
+	tabs.settings = dofile(menupath .. DIR_DELIM .. "tab_settings.lua")
+	tabs.content  = dofile(menupath .. DIR_DELIM .. "tab_content.lua")
+end
+
 tabs.credits  = dofile(menupath .. DIR_DELIM .. "tab_credits.lua")
-if menustyle == "simple" then
-	tabs.simple_main = dofile(menupath .. DIR_DELIM .. "tab_simple_main.lua")
-else
-	tabs.local_game = dofile(menupath .. DIR_DELIM .. "tab_local.lua")
-	tabs.play_online = dofile(menupath .. DIR_DELIM .. "tab_online.lua")
+tabs.local_default_game = dofile(menupath .. DIR_DELIM .. "tab_local_default.lua")
+tabs.local_game = dofile(menupath .. DIR_DELIM .. "tab_local.lua")
+tabs.play_online = dofile(menupath .. DIR_DELIM .. "tab_online.lua")
+local hpath = menupath .. DIR_DELIM .. "tab_hosting.lua"
+local hosting = io.open(hpath, "r")
+if hosting then
+	tabs.hosting = dofile(hpath)
+	io.close(hosting)
 end
 
 --------------------------------------------------------------------------------
@@ -67,92 +75,77 @@ local function main_event_handler(tabview, event)
 end
 
 --------------------------------------------------------------------------------
-local function init_globals()
-	-- Init gamedata
-	gamedata.worldindex = 0
-
-	if menustyle == "simple" then
-		local world_list = core.get_worlds()
-		local world_index
-
-		local found_singleplayerworld = false
-		for i, world in ipairs(world_list) do
-			if world.name == "singleplayerworld" then
-				found_singleplayerworld = true
-				world_index = i
-				break
-			end
-		end
-
-		if not found_singleplayerworld then
-			core.create_world("singleplayerworld", 1)
-
-			world_list = core.get_worlds()
-
-			for i, world in ipairs(world_list) do
-				if world.name == "singleplayerworld" then
-					world_index = i
-					break
-				end
-			end
-		end
-
-		gamedata.worldindex = world_index
-	else
-		menudata.worldlist = filterlist.create(
-			core.get_worlds,
-			compare_worlds,
-			-- Unique id comparison function
-			function(element, uid)
-				return element.name == uid
-			end,
-			-- Filter function
-			function(element, gameid)
-				return element.gameid == gameid
-			end
-		)
-
-		menudata.worldlist:add_sort_mechanism("alphabetic", sort_worlds_alphabetic)
-		menudata.worldlist:set_sortmode("alphabetic")
-
-		if not core.settings:get("menu_last_game") then
-			local default_game = core.settings:get("default_game") or "minetest"
-			core.settings:set("menu_last_game", default_game)
-		end
-
-		mm_texture.init()
-	end
-
-	-- Create main tabview
+function menudata.init_tabs()
 	local tv_main = tabview_create("maintab", {x = 12, y = 5.4}, {x = 0, y = 0})
 
-	if menustyle == "simple" then
-		tv_main:add(tabs.simple_main)
-	else
-		tv_main:set_autosave_tab(true)
-		tv_main:add(tabs.local_game)
-		tv_main:add(tabs.play_online)
+	for i = 1, #pkgmgr.games do
+		if pkgmgr.games[i].name == "default" then
+			tv_main:add(tabs.local_default_game)
+			break
+		end
 	end
 
-	tv_main:add(tabs.content)
-	tv_main:add(tabs.settings)
+	for i = 1, #pkgmgr.games do
+		if pkgmgr.games[i].name ~= "default" then
+			tv_main:add(tabs.local_game)
+			break
+		end
+	end
+
+	tv_main:add(tabs.play_online)
+	if tabs.hosting then
+		tv_main:add(tabs.hosting)
+	end
+
+	if not mobile then
+		tv_main:set_autosave_tab(true)
+		tv_main:add(tabs.content)
+		tv_main:add(tabs.settings)
+	end
 	tv_main:add(tabs.credits)
 
 	tv_main:set_global_event_handler(main_event_handler)
 	tv_main:set_fixed_size(false)
 
-	if menustyle ~= "simple" then
+	if not mobile then
 		local last_tab = core.settings:get("maintab_LAST")
 		if last_tab and tv_main.current_tab ~= last_tab then
 			tv_main:set_tab(last_tab)
 		end
 	end
+
 	ui.set_default("maintab")
 	tv_main:show()
 
 	ui.update()
+end
 
-	core.sound_play("main_menu", true)
+--------------------------------------------------------------------------------
+local function init_globals()
+	-- Init gamedata
+	gamedata.worldindex = 0
+
+	menudata.worldlist = filterlist.create(
+		core.get_worlds,
+		compare_worlds,
+		-- Unique id comparison function
+		function(element, uid)
+			return element.name == uid
+		end,
+		-- Filter function
+		function(element, gameid)
+			return element.gameid == gameid
+		end
+	)
+
+	menudata.worldlist:add_sort_mechanism("alphabetic", sort_worlds_alphabetic)
+	menudata.worldlist:set_sortmode("alphabetic")
+
+	-- Create main tabview
+	core.set_clouds(false)
+	mm_texture.set_dirt_bg()
+	menudata.init_tabs()
+--	core.sound_play("main_menu", true)
 end
 
 init_globals()

mightyjoe781/MultiCraft2

builtin/mainmenu/init.lua
@@ -3,7 +3,7 @@
 --
 --This program is free software; you can redistribute it and/or modify
 --it under the terms of the GNU Lesser General Public License as published by
---the Free Software Foundation; either version 2.1 of the License, or
+--the Free Software Foundation; either version 3.0 of the License, or
 --(at your option) any later version.
 --
 --This program is distributed in the hope that it will be useful,
@@ -22,7 +22,7 @@ mt_color_dark_green = "#25C191"
 
 local menupath = core.get_mainmenu_path()
 local basepath = core.get_builtin_path()
-local menustyle = core.settings:get("main_menu_style")
+local mobile = PLATFORM == "Android" or PLATFORM == "iOS"
 defaulttexturedir = core.get_texturepath_share() .. DIR_DELIM .. "base" ..
 					DIR_DELIM .. "pack" .. DIR_DELIM
 
@@ -36,26 +36,34 @@ dofile(menupath .. DIR_DELIM .. "common.lua")
 dofile(menupath .. DIR_DELIM .. "pkgmgr.lua")
 dofile(menupath .. DIR_DELIM .. "textures.lua")
 
-dofile(menupath .. DIR_DELIM .. "dlg_config_world.lua")
-dofile(menupath .. DIR_DELIM .. "dlg_settings_advanced.lua")
-dofile(menupath .. DIR_DELIM .. "dlg_contentstore.lua")
-if menustyle ~= "simple" then
-	dofile(menupath .. DIR_DELIM .. "dlg_create_world.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_create_world.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_create_world_default.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_delete_world.lua")
+
+if not mobile then
+	dofile(menupath .. DIR_DELIM .. "dlg_config_world.lua")
 	dofile(menupath .. DIR_DELIM .. "dlg_delete_content.lua")
-	dofile(menupath .. DIR_DELIM .. "dlg_delete_world.lua")
+	dofile(menupath .. DIR_DELIM .. "dlg_contentstore.lua")
 	dofile(menupath .. DIR_DELIM .. "dlg_rename_modpack.lua")
+	dofile(menupath .. DIR_DELIM .. "dlg_settings_advanced.lua")
 end
 
 local tabs = {}
 
-tabs.settings = dofile(menupath .. DIR_DELIM .. "tab_settings.lua")
-tabs.content  = dofile(menupath .. DIR_DELIM .. "tab_content.lua")
+if not mobile then
+	tabs.settings = dofile(menupath .. DIR_DELIM .. "tab_settings.lua")
+	tabs.content  = dofile(menupath .. DIR_DELIM .. "tab_content.lua")
+end
+
 tabs.credits  = dofile(menupath .. DIR_DELIM .. "tab_credits.lua")
-if menustyle == "simple" then
-	tabs.simple_main = dofile(menupath .. DIR_DELIM .. "tab_simple_main.lua")
-else
-	tabs.local_game = dofile(menupath .. DIR_DELIM .. "tab_local.lua")
-	tabs.play_online = dofile(menupath .. DIR_DELIM .. "tab_online.lua")
+tabs.local_default_game = dofile(menupath .. DIR_DELIM .. "tab_local_default.lua")
+tabs.local_game = dofile(menupath .. DIR_DELIM .. "tab_local.lua")
+tabs.play_online = dofile(menupath .. DIR_DELIM .. "tab_online.lua")
+local hpath = menupath .. DIR_DELIM .. "tab_hosting.lua"
+local hosting = io.open(hpath, "r")
+if hosting then
+	tabs.hosting = dofile(hpath)
+	io.close(hosting)
 end
 
 --------------------------------------------------------------------------------
@@ -67,92 +75,77 @@ local function main_event_handler(tabview, event)
 end
 
 --------------------------------------------------------------------------------
-local function init_globals()
-	-- Init gamedata
-	gamedata.worldindex = 0
-
-	if menustyle == "simple" then
-		local world_list = core.get_worlds()
-		local world_index
-
-		local found_singleplayerworld = false
-		for i, world in ipairs(world_list) do
-			if world.name == "singleplayerworld" then
-				found_singleplayerworld = true
-				world_index = i
-				break
-			end
-		end
-
-		if not found_singleplayerworld then
-			core.create_world("singleplayerworld", 1)
-
-			world_list = core.get_worlds()
-
-			for i, world in ipairs(world_list) do
-				if world.name == "singleplayerworld" then
-					world_index = i
-					break
-				end
-			end
-		end
-
-		gamedata.worldindex = world_index
-	else
-		menudata.worldlist = filterlist.create(
-			core.get_worlds,
-			compare_worlds,
-			-- Unique id comparison function
-			function(element, uid)
-				return element.name == uid
-			end,
-			-- Filter function
-			function(element, gameid)
-				return element.gameid == gameid
-			end
-		)
-
-		menudata.worldlist:add_sort_mechanism("alphabetic", sort_worlds_alphabetic)
-		menudata.worldlist:set_sortmode("alphabetic")
-
-		if not core.settings:get("menu_last_game") then
-			local default_game = core.settings:get("default_game") or "minetest"
-			core.settings:set("menu_last_game", default_game)
-		end
-
-		mm_texture.init()
-	end
-
-	-- Create main tabview
+function menudata.init_tabs()
 	local tv_main = tabview_create("maintab", {x = 12, y = 5.4}, {x = 0, y = 0})
 
-	if menustyle == "simple" then
-		tv_main:add(tabs.simple_main)
-	else
-		tv_main:set_autosave_tab(true)
-		tv_main:add(tabs.local_game)
-		tv_main:add(tabs.play_online)
+	for i = 1, #pkgmgr.games do
+		if pkgmgr.games[i].id == "default" then
+			tv_main:add(tabs.local_default_game)
+			break
+		end
 	end
 
-	tv_main:add(tabs.content)
-	tv_main:add(tabs.settings)
+	for i = 1, #pkgmgr.games do
+		if pkgmgr.games[i].id ~= "default" then
+			tv_main:add(tabs.local_game)
+			break
+		end
+	end
+
+	tv_main:add(tabs.play_online)
+	if tabs.hosting then
+		tv_main:add(tabs.hosting)
+	end
+
+	if not mobile then
+		tv_main:set_autosave_tab(true)
+		tv_main:add(tabs.content)
+		tv_main:add(tabs.settings)
+	end
 	tv_main:add(tabs.credits)
 
 	tv_main:set_global_event_handler(main_event_handler)
 	tv_main:set_fixed_size(false)
 
-	if menustyle ~= "simple" then
+	if not mobile then
 		local last_tab = core.settings:get("maintab_LAST")
 		if last_tab and tv_main.current_tab ~= last_tab then
 			tv_main:set_tab(last_tab)
 		end
 	end
+
 	ui.set_default("maintab")
 	tv_main:show()
 
 	ui.update()
+end
 
-	core.sound_play("main_menu", true)
+--------------------------------------------------------------------------------
+local function init_globals()
+	-- Init gamedata
+	gamedata.worldindex = 0
+
+	menudata.worldlist = filterlist.create(
+		core.get_worlds,
+		compare_worlds,
+		-- Unique id comparison function
+		function(element, uid)
+			return element.name == uid
+		end,
+		-- Filter function
+		function(element, gameid)
+			return element.gameid == gameid
+		end
+	)
+
+	menudata.worldlist:add_sort_mechanism("alphabetic", sort_worlds_alphabetic)
+	menudata.worldlist:set_sortmode("alphabetic")
+
+	-- Create main tabview
+	core.set_clouds(false)
+	mm_texture.set_dirt_bg()
+	menudata.init_tabs()
+--	core.sound_play("main_menu", true)
 end
 
 init_globals()