897c7c206f5c0cd3879ed27fa714f9e4fb8b3f5d
rubenwardy <rw@rubenwardy.com>
2021-10-15 17:14:48 +0100

    Use scoped app storage on Android (#11466) From November 2021, the Play Store will no longer be accepting apps which use the deprecated getExternalStorageDirectory() API. Therefore, this commit replaces uses of deprecated API with the new scoped API (`getExternalFilesDir()` and `getExternalCacheDir()`). It also provides a temporary migration to move user data from the shared external directory to new storage. Fixes #2097,  #11417 and #11118

--- android/app/src/main/java/com/multicraft/game/UnzipService.java
+++ android/app/src/main/java/com/multicraft/game/UnzipService.java
@@ -24,15 +24,21 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
 import com.bugsnag.android.Bugsnag;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.zip.ZipEntry;
@@ -46,33 +52,62 @@ import static com.multicraft.game.helpers.Utilities.getZipsFromAssets;
 
 public class UnzipService extends IntentService {
 	public static final String ACTION_UPDATE = "com.multicraft.game.UPDATE";
-	public static final String EXTRA_KEY_IN_FILE = "com.multicraft.game.file";
-	public static final String ACTION_PROGRESS = "com.multicraft.game.progress";
-	public static final String ACTION_FAILURE = "com.multicraft.game.failure";
-	public static final int UNZIP_SUCCESS = -1;
-	public static final int UNZIP_FAILURE = -2;
+	public static final String ACTION_PROGRESS = "com.multicraft.game.PROGRESS";
+	public static final String ACTION_PROGRESS_MESSAGE = "com.multicraft.game.PROGRESS_MESSAGE";
+	public static final String ACTION_FAILURE = "com.multicraft.game.FAILURE";
+	public static final int SUCCESS = -1;
+	public static final int FAILURE = -2;
+	public static final int INDETERMINATE = -3;
 	private final int id = 1;
 	private NotificationManager mNotifyManager;
 	private String failureMessage;
 	private boolean isSuccess = true;
 
+	private static boolean isRunning = false;
+	public static synchronized boolean getIsRunning() {
+		return isRunning;
+	}
+	private static synchronized void setIsRunning(boolean v) {
+		isRunning = v;
+	}
+
 	public UnzipService() {
 		super("com.multicraft.game.UnzipService");
 	}
 
-	private void isDir(String dir, String unzipLocation) {
-		File f = new File(unzipLocation, dir);
-		if (!f.mkdirs() && !f.isDirectory())
-			Bugsnag.leaveBreadcrumb(f + " (destination) folder was not created");
-	}
-
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		createNotification();
-		unzip(intent);
+		Notification.Builder notificationBuilder = createNotification();
+		final File zipFile = new File(getCacheDir(), "Minetest.zip");
+		try {
+			setIsRunning(true);
+			File userDataDirectory = Utils.getUserDataDirectory(this);
+			if (userDataDirectory == null) {
+				throw new IOException("Unable to find user data directory");
+			}
+
+			try (InputStream in = this.getAssets().open(zipFile.getName())) {
+				try (OutputStream out = new FileOutputStream(zipFile)) {
+					int readLen;
+					byte[] readBuffer = new byte[16384];
+					while ((readLen = in.read(readBuffer)) != -1) {
+						out.write(readBuffer, 0, readLen);
+					}
+				}
+			}
+
+			migrate(notificationBuilder, userDataDirectory);
+			unzip(notificationBuilder, zipFile, userDataDirectory);
+		} catch (IOException e) {
+			isSuccess = false;
+			failureMessage = e.getLocalizedMessage();
+		} finally {
+			setIsRunning(false);
+			zipFile.delete();
+		}
 	}
 
-	private void createNotification() {
+	private Notification.Builder createNotification() {
 		// There are hardcoding only for show it's just strings
 		String name = "com.multicraft.game";
 		String channelId = "MultiCraft channel"; // The user-visible name of the channel.
@@ -97,10 +132,20 @@ public class UnzipService extends IntentService {
 			builder = new Notification.Builder(this, channelId);
 		} else
 			builder = new Notification.Builder(this);
+		}
+
+		Intent notificationIntent = new Intent(this, MainActivity.class);
+		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+			| Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		PendingIntent intent = PendingIntent.getActivity(this, 0,
+			notificationIntent, 0);
+
 		builder.setContentTitle(getString(R.string.notification_title))
 				.setContentText(getString(R.string.notification_description))
 				.setSmallIcon(R.drawable.update);
+
 		mNotifyManager.notify(id, builder.build());
+		return builder;
 	}
 
 	private void unzip(Intent intent) {
@@ -111,7 +156,10 @@ public class UnzipService extends IntentService {
 			zips = getZipsFromAssets(this);
 		String cacheDir = getCacheDir().toString();
 		int per = 0;
-		int size = getSummarySize(zips, cacheDir);
+		int size;
+		try (ZipFile zipSize = new ZipFile(zipFile)) {
+			size = zipSize.size();
+		}
 		byte[] readBuffer = new byte[8192];
 		for (String zip : zips) {
 			String location = getLocationByZip(this, zip);
@@ -144,30 +192,81 @@ public class UnzipService extends IntentService {
 		}
 	}
 
-	private void publishProgress(int progress) {
-		Intent intentUpdate = new Intent(ACTION_UPDATE);
-		intentUpdate.putExtra(ACTION_PROGRESS, progress);
-		if (!isSuccess) intentUpdate.putExtra(ACTION_FAILURE, failureMessage);
-		sendBroadcast(intentUpdate);
+	void moveFileOrDir(@NonNull File src, @NonNull File dst) throws IOException {
+		try {
+			Process p = new ProcessBuilder("/system/bin/mv",
+				src.getAbsolutePath(), dst.getAbsolutePath()).start();
+			int exitcode = p.waitFor();
+			if (exitcode != 0)
+				throw new IOException("Move failed with exit code " + exitcode);
+		} catch (InterruptedException e) {
+			throw new IOException("Move operation interrupted");
+		}
 	}
 
-	private int getSummarySize(ArrayList<String> zips, String path) {
-		int size = 0;
-		for (String z : zips) {
-			try {
-				ZipFile zipFile = new ZipFile(new File(path, z));
-				size += zipFile.size();
-			} catch (IOException e) {
-				Bugsnag.notify(e);
+	boolean recursivelyDeleteDirectory(@NonNull File loc) {
+		try {
+			Process p = new ProcessBuilder("/system/bin/rm", "-rf",
+				loc.getAbsolutePath()).start();
+			return p.waitFor() == 0;
+		} catch (IOException | InterruptedException e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Migrates user data from deprecated external storage to app scoped storage
+	 */
+	private void migrate(Notification.Builder notificationBuilder, File newLocation) throws IOException {
+		File oldLocation = new File(Environment.getExternalStorageDirectory(), "Minetest");
+		if (!oldLocation.isDirectory())
+			return;
+
+		publishProgress(notificationBuilder, R.string.migrating, 0);
+		newLocation.mkdir();
+
+		String[] dirs = new String[] { "worlds", "games", "mods", "textures", "client" };
+		for (int i = 0; i < dirs.length; i++) {
+			publishProgress(notificationBuilder, R.string.migrating, 100 * i / dirs.length);
+			File dir = new File(oldLocation, dirs[i]), dir2 = new File(newLocation, dirs[i]);
+			if (dir.isDirectory() && !dir2.isDirectory()) {
+				moveFileOrDir(dir, dir2);
 			}
 		}
-		return size;
+
+		for (String filename : new String[] { "minetest.conf" }) {
+			File file = new File(oldLocation, filename), file2 = new File(newLocation, filename);
+			if (file.isFile() && !file2.isFile()) {
+				moveFileOrDir(file, file2);
+			}
+		}
+
+		recursivelyDeleteDirectory(oldLocation);
+	}
+
+	private void publishProgress(@Nullable  Notification.Builder notificationBuilder, @StringRes int message, int progress) {
+		Intent intentUpdate = new Intent(ACTION_UPDATE);
+		intentUpdate.putExtra(ACTION_PROGRESS, progress);
+		intentUpdate.putExtra(ACTION_PROGRESS_MESSAGE, message);
+		if (!isSuccess)
+			intentUpdate.putExtra(ACTION_FAILURE, failureMessage);
+		sendBroadcast(intentUpdate);
+
+		if (notificationBuilder != null) {
+			notificationBuilder.setContentText(getString(message));
+			if (progress == INDETERMINATE) {
+				notificationBuilder.setProgress(100, 50, true);
+			} else {
+				notificationBuilder.setProgress(100, progress, false);
+			}
+			mNotifyManager.notify(id, notificationBuilder.build());
+		}
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		mNotifyManager.cancel(id);
-		publishProgress(isSuccess ? UNZIP_SUCCESS : UNZIP_FAILURE);
+		publishProgress(null, R.string.loading, isSuccess ? SUCCESS : FAILURE);
 	}
 }
